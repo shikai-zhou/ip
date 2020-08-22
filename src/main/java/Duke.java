9f546cef20b31616ca5d5e012b41dda55544c30e
shikai-zhou <shikai55@gmail.com>
2020-08-22 18:23:54 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,29 +3,36 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String[] arr ;
-        arr = new String[100];
+        Task[] arr ;
+        arr = new Task[100];
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         int i = 0;
         while (true) {
             String input = in.nextLine();
             if (input.equals("bye")) break;
-            if (input.equals("list")) list(i, arr);
-            else
-            {
-                arr[i] = input;
-                System.out.println("added: " + arr[i]);
+            else if (input.equals("list")) list(i, arr);
+            else if (input.matches("done(.*)")) done(input.charAt(input.length()-1), arr);
+            else {
+                arr[i] = new Task(input);
+                System.out.println("added: " + arr[i].getDescription());
                 i++;
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
-    public static void list(int size, String[] arr)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            System.out.println((j+1)+". "+arr[j]);
+
+    public static void list(int size, Task[] arr) {
+        System.out.println("Here are the tasks in your list:");
+        for (int j = 0; j < size; j++) {
+            System.out.println((j+1)+".["+arr[j].getStatusIcon()+"] "+arr[j].getDescription());
         }
+    }
+
+    public static void done(char n, Task[] arr) {
+        int i = Character.getNumericValue(n) - 1; //the user counts from 1 instead of 0
+        arr[i].setAsDone();
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println("  ["+arr[i].getStatusIcon()+"] "+arr[i].getDescription());
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,7 @@
 public class Task {
     protected String description;
     protected boolean isDone;
-
+    //constructor
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -11,5 +11,12 @@ public class Task {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setAsDone() {
+        this.isDone = true;
+    }
     //...
 }
