400b9809a31bc90de489f7a99308172cef45fb0a
shikai-zhou <shikai55@gmail.com>
2020-08-22 19:00:33 +0800

    fixed bug

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,36 +3,48 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Task[] arr ;
-        arr = new Task[100];
+        Task[] tasks ;
+        tasks = new Task[100];
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         int i = 0;
         while (true) {
             String input = in.nextLine();
             if (input.equals("bye")) break;
-            else if (input.equals("list")) list(i, arr);
-            else if (input.matches("done(.*)")) done(input.charAt(input.length()-1), arr);
+            else if (input.equals("list")){
+                list(i, tasks);
+            }
+            else if (input.matches("done(.*)")) {
+                done(input, tasks);
+            }
             else {
-                arr[i] = new Task(input);
-                System.out.println("added: " + arr[i].getDescription());
+                tasks[i] = new Task(input);
+                System.out.println("added: " + tasks[i].getDescription());
                 i++;
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public static void list(int size, Task[] arr) {
+    public static void list(int size, Task[] tasks) {
         System.out.println("Here are the tasks in your list:");
         for (int j = 0; j < size; j++) {
-            System.out.println((j+1)+".["+arr[j].getStatusIcon()+"] "+arr[j].getDescription());
+            System.out.println((j+1)+".["+tasks[j].getStatusIcon()+"] "+tasks[j].getDescription());
         }
     }
 
-    public static void done(char n, Task[] arr) {
-        int i = Character.getNumericValue(n) - 1; //the user counts from 1 instead of 0
-        arr[i].setAsDone();
+    public static void done(String in, Task[] tasks) {
+        int i = 0;
+        for (int j = 1; j < in.length(); j++) {
+            char n = in.charAt(in.length() - j);
+            if (n == ' ') {
+                break;
+            }
+            i += (Character.getNumericValue(n)*Math.pow(10,(j-1)));
+        }
+        i = i - 1; //the user counts from 1 instead of 0
+        tasks[i].setAsDone();
         System.out.println("Nice! I've marked this task as done: ");
-        System.out.println("  ["+arr[i].getStatusIcon()+"] "+arr[i].getDescription());
+        System.out.println("  ["+tasks[i].getStatusIcon()+"] "+tasks[i].getDescription());
     }
 }
