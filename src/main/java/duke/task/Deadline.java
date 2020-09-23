337d6c6e467b5b578314f514f1dd187a46a1805e
shikai-zhou <shikai55@gmail.com>
2020-09-23 18:57:03 +0800

    formatted date

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,7 +1,9 @@
 package duke.task;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DateTimeParseException;
+import java.util.Locale;
 
 public class Deadline extends Task {
     protected LocalDate date;
@@ -9,8 +11,11 @@ public class Deadline extends Task {
     protected boolean isDate;
     public Deadline(String description, String by) {
         super(description);
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("dd-MMM-yyyy")
+                // create formatter (use English Locale to parse month names)
+                .toFormatter(Locale.ENGLISH);;
         try {
-            this.date = LocalDate.parse(by);
+            this.date = LocalDate.parse(by, formatter);
             isDate = true;
         } catch (DateTimeParseException e) {
             this.by = by;
@@ -21,6 +26,6 @@ public class Deadline extends Task {
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " +
-                (isDate?date.format(DateTimeFormatter.ofPattern("d MMM YYYY")):by) + ")";
+                (isDate?date.format(DateTimeFormatter.ofPattern("d MMM yyyy")):by) + ")";
     }
 }
--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -2,7 +2,9 @@ package duke.task;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DateTimeParseException;
+import java.util.Locale;
 
 public class Event extends Task {
     protected LocalDate date;
@@ -11,7 +13,10 @@ public class Event extends Task {
     public Event (String description, String at) {
         super(description);
         try {
-            this.date = LocalDate.parse(at);
+            DateTimeFormatter formatter = new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("dd-MMM-yyyy")
+                    // create formatter (use English Locale to parse month names)
+                    .toFormatter(Locale.ENGLISH);;
+            this.date = LocalDate.parse(at, formatter);
             isDate = true;
         } catch (DateTimeParseException e) {
             this.at = at;
@@ -22,6 +27,6 @@ public class Event extends Task {
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (at: " +
-                (isDate?date.format(DateTimeFormatter.ofPattern("d MMM YYYY")):at) + ")";
+                (isDate?date.format(DateTimeFormatter.ofPattern("d MMM yyyy")):at) + ")";
     }
 }
