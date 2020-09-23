5e96809cad621e877970567d995d51c3c8645137
shikai-zhou <shikai55@gmail.com>
2020-09-23 16:34:09 +0800

    added time

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,15 +1,26 @@
 package duke.task;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-
+    protected LocalDate date;
     protected String by;
-
+    protected boolean isDate;
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.date = LocalDate.parse(by);
+            isDate = true;
+        } catch (DateTimeParseException e) {
+            this.by = by;
+            isDate = false;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " +
+                (isDate?date.format(DateTimeFormatter.ofPattern("d MMM YYYY")):by) + ")";
     }
 }
--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -1,14 +1,27 @@
 package duke.task;
-public class Event extends Task {
-    protected String at;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Event extends Task {
+    protected LocalDate date;
+    protected String at;
+    protected boolean isDate;
     public Event (String description, String at) {
         super(description);
-        this.at = at;
+        try {
+            this.date = LocalDate.parse(at);
+            isDate = true;
+        } catch (DateTimeParseException e) {
+            this.at = at;
+            isDate = false;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " +
+                (isDate?date.format(DateTimeFormatter.ofPattern("d MMM YYYY")):at) + ")";
     }
 }
